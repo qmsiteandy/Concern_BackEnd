7d49f9f23b28f6ef9093ce0325c2ff22b3328475
qmsiteandy <ht93084@gmail.com>
2022-02-19 16:54:04 +0800

    update clear unused script

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const http = require("http");
 
 require('dotenv').config();
 
+//MongoDB連線
 mongoose.connect(process.env.MONGODB_ATLAS_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -29,19 +30,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+//加入API Routers
 const teacherRouter = require("./routers/teacherRouter");
 const courseRouter = require("./routers/courseRouter");
 const classroomRouter = require("./routers/classroomRouter");
 const studentRouter = require("./routers/studentRouter");
 
-
+//設定API路徑
 app.use("/api/teacher", teacherRouter);
 app.use("/api/course", courseRouter);
 app.use("/api/classroom", classroomRouter);
 app.use("/api/student", studentRouter);
+//伺服器回應
+app.get("/", (req, res) => { res.send("Server is ready"); }); 
 
-app.get("/", (req, res) => { res.send("Server is ready！！"); }); 
-
+//設定監聽PORT
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
